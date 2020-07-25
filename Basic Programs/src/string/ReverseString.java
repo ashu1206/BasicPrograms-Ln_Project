3bfa665a316993d6534d25c9b4f64b0c4453f8ad
ashu1206 <ashukshukla612@gmail.com>
2020-07-26 00:35:49 +0530

    ReverseString Added

---
+++ Basic Programs/src/string/ReverseString.java
@@ -0,0 +1,15 @@
+package string;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str = "India is my country";
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(str);
+		//buffer.reverse();
+		System.out.println(buffer.reverse());
+
+	}
+
+}
